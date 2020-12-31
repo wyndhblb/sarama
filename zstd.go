187d22f2e74d09e9e35f8d289b612bf3cceb7ad6
bo <bblanton@asapp.com>
2020-12-31 09:12:16 -0700

    try pools

--- zstd.go
+++ zstd.go
@@ -1,14 +1,35 @@
 package sarama
 
 import (
-	"github.com/DataDog/zstd"
+	"bytes"
+	"github.com/klauspost/compress/zstd"
+	"sync"
 )
 
+var (
+	zstdWriterPool = sync.Pool{
+		New: func() interface{} {
+			p, _ := zstd.NewWriter(nil)
+			return p
+		},
+	}
+	zstdReaderPool = sync.Pool{
+		New: func() interface{} {
+			p, _ := zstd.NewReader(nil)
+			return p
+		},
+	}
+)
 
 func zstdDecompress(dst, src []byte) ([]byte, error) {
-	return zstd.Decompress(dst, src)
+	reader := zstdReaderPool.Get().(*zstd.Decoder)
+	defer zstdReaderPool.Put(reader)
+	return reader.DecodeAll(src, dst)
 }
 
 func zstdCompress(dst, src []byte) ([]byte, error) {
-	return zstd.Compress(dst, src)
+	writer := zstdWriterPool.Get().(*zstd.Encoder)
+	defer zstdWriterPool.Put(writer)
+	bs := writer.EncodeAll(src, dst)
+	return bs, nil
 }
