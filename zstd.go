02b8a8d40628cb99644c1a100018e0c41a5b4b42
bo <bblanton@asapp.com>
2021-01-02 08:36:39 -0700

    dead code

--- zstd.go
+++ zstd.go
@@ -31,24 +31,24 @@ var (
 func zstdDecompress(dst, src []byte) ([]byte, error) {
 
 	return zstdReader.DecodeAll(src, dst)
-
-	reader := zstdReaderPool.Get().(*zstd.Decoder)
-	if dst == nil{
-		bs := zstdBytePool.Get().(*bytes.Buffer)
-		bs.Reset()
-		dst = bs.Bytes()
-		zstdBytePool.Put(bs)
-	}
-	defer zstdReaderPool.Put(reader)
-	return reader.DecodeAll(src, dst)
+	//
+	//reader := zstdReaderPool.Get().(*zstd.Decoder)
+	//if dst == nil{
+	//	bs := zstdBytePool.Get().(*bytes.Buffer)
+	//	bs.Reset()
+	//	dst = bs.Bytes()
+	//	zstdBytePool.Put(bs)
+	//}
+	//defer zstdReaderPool.Put(reader)
+	//return reader.DecodeAll(src, dst)
 }
 
 func zstdCompress(dst, src []byte) ([]byte, error) {
 	bs := zstdWriter.EncodeAll(src, dst)
 	return bs, nil
-
-	writer := zstdWriterPool.Get().(*zstd.Encoder)
-	defer zstdWriterPool.Put(writer)
-	bs := writer.EncodeAll(src, dst)
-	return bs, nil
+	//
+	//writer := zstdWriterPool.Get().(*zstd.Encoder)
+	//defer zstdWriterPool.Put(writer)
+	//bs := writer.EncodeAll(src, dst)
+	//return bs, nil
 }
