087f4e3676d21a7490c1675b14557c768f0fb0a6
bo <bblanton@asapp.com>
2021-01-07 09:22:24 -0700

    Performance improvement
    the lock in that once per decode/encode is expensive, so just do it once at init

--- zstd.go
+++ zstd.go
@@ -1,28 +1,25 @@
 package sarama
 
 import (
-	"sync"
-
 	"github.com/klauspost/compress/zstd"
+	"sync"
 )
 
-var (
-	zstdDec *zstd.Decoder
-	zstdEnc *zstd.Encoder
+var zstdDec *zstd.Decoder
+var zstdEnc *zstd.Encoder
+var zstdOnce = sync.Once{}
 
-	zstdEncOnce, zstdDecOnce sync.Once
-)
+func init(){
+	zstdOnce.Do(func() {
+		zstdDec, _ = zstd.NewReader(nil)
+		zstdEnc, _ = zstd.NewWriter(nil, zstd.WithZeroFrames(true))
+	})
+}
 
 func zstdDecompress(dst, src []byte) ([]byte, error) {
-	zstdDecOnce.Do(func() {
-		zstdDec, _ = zstd.NewReader(nil)
-	})
 	return zstdDec.DecodeAll(src, dst)
 }
 
 func zstdCompress(dst, src []byte) ([]byte, error) {
-	zstdEncOnce.Do(func() {
-		zstdEnc, _ = zstd.NewWriter(nil, zstd.WithZeroFrames(true))
-	})
 	return zstdEnc.EncodeAll(src, dst), nil
 }
